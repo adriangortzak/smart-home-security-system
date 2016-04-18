451fbbdd9995517d96fe166ab4ab88f9fde1543b
Adrian Gortzak <addegor95@gmail.com>
2016-04-18 15:48:37 +0200

    started with testing

---
+++ src/org/gortz/alarm/Test/View/ViewTest.java
@@ -0,0 +1,19 @@
+package org.gortz.alarm.Test.View;
+
+import org.junit.Test;
+
+/**
+ * Created by adrian on 18/04/16.
+ */
+public class ViewTest {
+    @Test
+    public void start() throws Exception {
+
+    }
+
+    @Test
+    public void print() throws Exception {
+
+    }
+
+}
