29146d43517be3185286ffd05e00300f87af181f
Adrian Gortzak <addegor95@gmail.com>
2016-04-18 15:58:56 +0200

    added tellstick test

---
+++ src/org/gortz/alarm/Test/Model/TellstickDuoTest.java
@@ -0,0 +1,36 @@
+package org.gortz.alarm.Test.Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by adrian on 18/04/16.
+ */
+public class TellstickDuoTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void turnOn() throws Exception {
+
+    }
+
+    @Test
+    public void turnOff() throws Exception {
+
+    }
+
+    @Test
+    public void close() throws Exception {
+
+    }
+
+}
