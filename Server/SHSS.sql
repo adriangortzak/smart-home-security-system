ed49cac7c832bfd106626aa51ec074b941d094bf
Adrian Gortzak <addegor95@gmail.com>
2016-04-19 09:48:43 +0200

    database script

--- Server/SHSS.sql
+++ Server/SHSS.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.0
+-- version 4.2.12deb2+deb8u1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 16, 2016 at 07:18 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.5
+-- Generation Time: Apr 19, 2016 at 07:44 AM
+-- Server version: 5.5.44-0+deb8u1
+-- PHP Version: 5.6.19-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `SHSS`
@@ -23,45 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `alarm_status`
+-- Table structure for table `alarmStatus`
 --
 
-CREATE TABLE `alarm_status` (
-  `id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `Status` tinyint(1) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `alarmStatus` (
+`id` int(11) NOT NULL,
+  `activeAlarm` tinyint(1) NOT NULL,
+  `lastChange` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `alarm_status`
+-- Indexes for table `alarmStatus`
 --
-ALTER TABLE `alarm_status`
-  ADD UNIQUE KEY `id_3` (`id`),
-  ADD KEY `id` (`id`),
-  ADD KEY `id_2` (`id`);
+ALTER TABLE `alarmStatus`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `alarm_status`
+-- AUTO_INCREMENT for table `alarmStatus`
 --
-ALTER TABLE `alarm_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `alarmStatus`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
--- User create
-GRANT USAGE ON *.* TO 'SHSS'@'localhost' IDENTIFIED BY PASSWORD
-'*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19';
-
-GRANT ALL PRIVILEGES ON `SHSS`.* TO 'SHSS'@'localhost' WITH GRANT OPTION;
