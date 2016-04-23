c1d2b4f17fdc3ddc355ebdb884c6a9e88a2b448b
Jimmy <jimmy@fjallid.com>
2016-04-23 20:31:06 +0000

    Added class for encapsulating sensor data from temp/humidity sensor received by tellstick. Updated regular expression.

--- src/org/gortz/alarm/model/Sensors/Listen.java
+++ src/org/gortz/alarm/model/Sensors/Listen.java
@@ -4,7 +4,8 @@ import net.jstick.api.RawEvent;
 import net.jstick.api.RawEventListener;
 import net.jstick.api.Tellstick;
 import org.gortz.alarm.model.CommandObject;
-import org.gortz.alarm.model.Logger;
+import org.gortz.alarm.model.Loggers.Logger;
+import org.gortz.alarm.model.SensorData;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -37,12 +38,22 @@ public class Listen implements Runnable{
             @Override
             public void eventReceived(RawEvent rawEvent) {
                 String temp = rawEvent.getData();
-                String regexPattern = "class:(?<type>command|sensor).*protocol:(?<protcol>\\w+);model:(?<model>\\w+);(?:house:(?<house>\\d+);unit:(?<unit>\\d);)?(?:group:(?<group>\\d);)?(?:code:(?<code>\\w+);)?method:(?<method>[a-z]+);";
+                String regexPattern = "class:(?<type>command|sensor);protocol:(?<protcol>\\w+);(?:id:(?<id>\\d+);)?model:(?<model>\\w+);(?:(?:house:(?<house>\\d+);unit:(?<unit>\\d);)?(?:group:(?<group>\\d);)?(?:code:(?<code>\\w+);)?method:(?<method>[a-z]+);)?(?:humidity:(?<humidity>\\d+);temp:(?<temp>\\d+\\.\\d+);)?";
                 Pattern pattern = java.util.regex.Pattern.compile(regexPattern);
                 Matcher matcher = pattern.matcher(temp);
                 if(matcher.matches()){
-                    CommandObject c = new CommandObject(matcher.group(2),matcher.group(3),matcher.group(4),matcher.group(5),matcher.group(6),matcher.group(7), matcher.group(8));
-                    myLogger.write(c.getMethod(),3);
+                    if(matcher.group(1).equals("command")){
+                        CommandObject c = new CommandObject(matcher.group(2),matcher.group(4),matcher.group(5),matcher.group(6),matcher.group(7),matcher.group(8), matcher.group(9));
+                        myLogger.write(c.getMethod(),3);
+                    }
+                    else if(matcher.group(1).equals("sensor")){
+                        SensorData s = new SensorData(matcher.group(2),matcher.group(3),matcher.group(4),matcher.group(10), matcher.group(11));
+                        myLogger.write("Current temp: " + s.getTemp() + " degrees Celsius and current humidity: " + s.gethumidity() + "%",3);
+                    }
+                    else{
+                        myLogger.write("Received unknown sensor input", 3);
+                    }
+
                 }
             }
         };
