8631d4eca97c778b348410c89d53180ffb2f63f4
Jimmy <jimmy@fjallid.com>
2016-04-26 20:54:04 +0000

    Updated compareTo to account for difference between tellstick snooping and config file e.g. selflearning and selflearning-switch are the same in arctech.

--- src/main/java/org/gortz/alarm/model/CommandObject.java
+++ src/main/java/org/gortz/alarm/model/CommandObject.java
@@ -4,13 +4,13 @@ package org.gortz.alarm.model;
  * Created by jimmy on 4/23/16.
  */
 public class CommandObject {
-    private String protocol;
-    private String model;
-    private String house;
-    private String unit;
-    private String group;
-    private String code;
-    private String method;
+    private String protocol = "Null";
+    private String model = "Null";
+    private String house = "Null";
+    private String unit = "Null";
+    private String group = "Null";
+    private String code = "Null";
+    private String method = "Null";
 
     public CommandObject(String protocol, String model, String house, String unit, String group, String code, String method){
         this.protocol = protocol;
@@ -50,21 +50,33 @@ public class CommandObject {
         return method;
     }
 
+    public void print(){
+        System.out.println("------------------Printed CommandObject--------------------");
+        System.out.println("Protocol: " + this.getProtocol());
+        System.out.println("Model: " + this.getModel());
+        if(!this.getHouse().equals("Null")) System.out.println("House: " + this.getHouse());
+        if(!this.getUnit().equals("Null")) System.out.println("Unit: " + this.getUnit());
+        if(!this.getGroup().equals("Null")) System.out.println("Group: " + this.getGroup());
+        if(!this.getCode().equals("Null")) System.out.println("Code: " + this.getCode());
+        System.out.println("-----------------------------------------------------------");
+    }
+
     public boolean compareTo(CommandObject co){
         if(!this.getProtocol().equals(co.getProtocol())) {
             return false;
         }
-        else if(!this.getModel().equals(co.getModel())){
-            return false;
+        //Problem with config file requiring model: "selflearning-switch" while tellstick receives only "selflearning" from messages for arctech.
+        else if( !(this.getModel().equals("selflearning") && (co.getModel().equals("selflearning-switch") || co.getModel().equals("selflearning"))) ){
+            if(!this.getModel().equals(co.getModel())){
+                return false;
+            }
         }
         switch (this.getProtocol()){
-            case "arcteck":
+            case "arctech":
                 if(!this.getHouse().equals(co.getHouse())){
-
                     return false;
                 }
                 else if(!this.getUnit().equals(co.getUnit())){
-
                     return false;
                 }
                 return true;
