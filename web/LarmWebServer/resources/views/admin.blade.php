2e09f4bbe90408f7a2928b5c104551447410abf1
adddrian <adriangor95>
2016-04-28 20:21:43 +0200

    new

--- web/LarmWebServer/resources/views/admin.blade.php
+++ web/LarmWebServer/resources/views/admin.blade.php
@@ -5,6 +5,7 @@
 
 
 @section('main_content')
+<meta name="csrf-token" content="{{ Session::token() }}">
 <h1 style="text-align: center;"><b>Admin</b></h1>
 
 <div class="col-lg-12">
@@ -12,12 +13,12 @@
 <h4>Create new user</h4>
 <hr>
                         {!! csrf_field() !!}
-
+<div>
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
                             </div>
                         </div>
 
@@ -25,7 +26,7 @@
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
                             </div>
                         </div>
 
@@ -33,13 +34,7 @@
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <input type="password" id="password1" class="form-control" name="password">
                             </div>
                         </div>
 
@@ -47,30 +42,29 @@
                             <label class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
+                                <input type="password" id="password2" class="form-control" name="password_confirmation">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="button" onclick="" class="btn btn-primary">
+                                <button type="button" onclick="registerUser()" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i>Register
                                 </button>
                             </div>
                         </div>
-                   <script>
-                       function () {
-                           $.post("register", {suggest: txt}, function(data){
-                               alert("Data: " + data);
-                           })
-                       }
-
-                   </script>
-
-
+</div>
 </div>
 </div>
 
+
+<script>
+    function registerUser() {
+        $.post("newUser", {name:$("#name").attr('value'),email: $("#email").attr('value'), password:$("#password").attr('value'),_token:$('meta[name=csrf-token]').attr('content')}, function(data){
+            alert("Data: " + data);
+        })
+    }
+</script>
 
 
 <div class="col-lg-12">
