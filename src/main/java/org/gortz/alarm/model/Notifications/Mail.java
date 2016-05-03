56524a95efede75a4cb2e36e5fb9c6dfa9100a0d
jfjallid <jimmy@fjallid.com>
2016-05-03 14:27:32 +0000

    First try to resolve problem with ssl connection

--- src/main/java/org/gortz/alarm/model/Notifications/Mail.java
+++ src/main/java/org/gortz/alarm/model/Notifications/Mail.java
@@ -53,6 +53,8 @@ public class Mail implements Notification {
                 });
         try {
             Message emailMessage = new MimeMessage(session);
+            System.out.println("email username is: " + username);
+            System.out.println("email password is: " + password);
             emailMessage.setFrom(new InternetAddress("II1302Group13@gmail.com"));
             emailMessage.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(recipient));
