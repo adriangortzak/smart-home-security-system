3f230ff0070635de34ce7c6279a85f9c9c83ece4
jfjallid <jimmy@fjallid.com>
2016-05-10 08:30:17 +0000

    fixed missing ""

--- web/LarmWebServer/resources/views/admin.blade.php
+++ web/LarmWebServer/resources/views/admin.blade.php
@@ -159,10 +159,10 @@ updateUserManagement();
 </div>
 <script>
     function cancelSensorEdit(id) {
-        document.getElementById( id + '-check').style.display = none;
-	document.getElementById( id + '-cancel').style.display = none;
-	document.getElementById( id + '-edit').style.display = inline;
-	document.getElementById( id + '-thrash').style.display = inline;
+        document.getElementById( id + '-check').style.display = "none";
+	document.getElementById( id + '-cancel').style.display = "none";
+	document.getElementById( id + '-edit').style.display = "inline";
+	document.getElementById( id + '-thrash').style.display = "inline";
 	getSensors();
 	
 	//Probably do some stuff to reload values from database or restore them from local copy while editing.
@@ -170,18 +170,18 @@ updateUserManagement();
     }
 
     function editSensor(id) {
-        document.getElementById( id + '-check').style.display = inline;
-	document.getElementById( id + '-cancel').style.display = inline;
-	document.getElementById( id + '-edit').style.display = none;
-	document.getElementById( id + '-thrash').style.display = none;
+        document.getElementById( id + '-check').style.display = "inline";
+	document.getElementById( id + '-cancel').style.display = "inline";
+	document.getElementById( id + '-edit').style.display = "none";
+	document.getElementById( id + '-thrash').style.display = "none";
 	// Set input for all fields. Hidden inputs?
     }
 
     function confirmSensorEdit(id) {
-        document.getElementById( id + '-check').style.display = none;
-	document.getElementById( id + '-cancel').style.display = none;
-	document.getElementById( id + '-edit').style.display = inline;
-	document.getElementById( id + '-thrash').style.display = inline;
+        document.getElementById( id + '-check').style.display = "none";
+	document.getElementById( id + '-cancel').style.display = "none";
+	document.getElementById( id + '-edit').style.display = "inline";
+	document.getElementById( id + '-thrash').style.display = "inline";
 	// Do some stuff to apply the changes...
 	// update database with new values and remove hidden inputs?
     }
