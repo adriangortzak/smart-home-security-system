f44e98b878a101e42b734808e9f41d68a5a1e3a5
Jimmy <jimmy@fjallid.com>
2016-05-19 22:02:23 +0000

    fixed pom file to include jstick api and update install script to install local maven repo.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "---------------------------------------------"
-echo "| starting to install Smart Home Larm System |"
+echo "| starting to install Smart Home Security System |"
 echo "---------------------------------------------"
 echo ""
-echo "Moving web config"
-cp   web/config/SmartHomeLarmSystem.conf /etc/apache2/conf-enabled/
-echo "IncludeOptional conf-enable/SmartHomeLarmSystem.conf" >> /etc/apache2/apache2.conf 
-echo "Restarting webserver"
-systemctl status apache2.service
+#echo "Moving web config"
+#cp   web/config/SmartHomeLarmSystem.conf /etc/apache2/conf-enabled/
+#echo "IncludeOptional conf-enable/SmartHomeLarmSystem.conf" >> /etc/apache2/apache2.conf 
+#echo "Restarting webserver"
+#systemctl status apache2.service
+echo "Installing net.jstick.api as local maven repository"
+mvn install:install-file -Dfile=Server/jstick-api-1.7.jar -DgroupId=net.jstick.api -DartifactId=jstick -Dversion=1.0 -Dpackaging=jar
